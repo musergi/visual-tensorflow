91afcc299f66d673d59694411b590c15e9e92815
musergi <sergi.mus@gmail.com>
2020-10-26 12:00:58 +0100

    Added TensorflowModel and Dataset

--- visualtensorflow/main/models.py
+++ visualtensorflow/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Dataset(models.Model):
+    name = models.CharField(max_length=200)
+    data_file = models.FileField(upload_to='datasets/')
+
+class TensorflowModel(models.Model):
+    name = models.CharField(max_length=200)
+    model_file = models.FileField(upload_to='models/')
