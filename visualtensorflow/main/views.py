e291547d2ac0a93e9c73c0abb25de543776d6211
musergi <sergi.mus@gmail.com>
2020-10-26 15:12:37 +0100

    Added dataset display

--- visualtensorflow/main/views.py
+++ visualtensorflow/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Dataset
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    context = {'datasets': Dataset.objects.all()}
+    return render(request, 'main/index.html', context)
